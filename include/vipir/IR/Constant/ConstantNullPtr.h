ff07b147af9bd7c8ff8d0d99ed03d1aabe45ea66
solar-mist <solarmistreal@gmail.com>
2024-05-02 21:30:09 +0000

    Add constant null pointer value

---
+++ include/vipir/IR/Constant/ConstantNullPtr.h
@@ -0,0 +1,37 @@
+// Copyright 2024 solar-mist
+
+/*
+ * ConstantNullPtr is a value that represents
+ * a pointer with no value
+*/
+
+#ifndef VIPIR_IR_CONSTANT_CONSTANT_NULL_PTR_H
+#define VIPIR_IR_CONSTANT_CONSTANT_NULL_PTR_H 1
+
+#include "vipir/IR/Value.h"
+
+#include <cstdint>
+
+namespace vipir
+{
+    class BasicBlock;
+    class ConstantNullPtr : public Value
+    {
+    friend class IRBuilder;
+    public:
+        static ConstantNullPtr* Get(Module& module, Type* type);
+
+        void print(std::ostream& stream) override;
+        std::string ident() const override;
+
+        bool isConstant() const override;
+
+    protected:
+        void emit(MC::Builder& builder) override;
+
+    private:
+        ConstantNullPtr(Module& module, Type* type);
+    };
+}
+
+#endif // VIPIR_IR_CONSTANT_CONSTANT_NULL_PTR_H
---
+++ src/vipir/IR/Constant/ConstantNullPtr.cpp
@@ -0,0 +1,41 @@
+// Copyright 2024 solar-mist
+
+
+#include "vipir/IR/Constant/ConstantNullPtr.h"
+
+#include "vipir/Module.h"
+
+#include "vasm/instruction/operand/Immediate.h"
+
+#include <format>
+
+namespace vipir
+{
+    ConstantNullPtr* ConstantNullPtr::Get(Module& module, Type* type)
+    {
+        ConstantNullPtr* constant = new ConstantNullPtr(module, type);
+
+        module.insertConstant(constant);
+
+        return constant;
+    }
+
+    void ConstantNullPtr::print(std::ostream& stream)
+    {
+    }
+
+    std::string ConstantNullPtr::ident() const
+    {
+        return std::format("{} null", mType->getName());
+    }
+
+    bool ConstantNullPtr::isConstant() const
+    {
+        return true;
+    }
+
+    void ConstantNullPtr::emit(MC::Builder& builder)
+    {
+        mEmittedValue = std::make_unique<instruction::Immediate>(0);
+    }
+}
